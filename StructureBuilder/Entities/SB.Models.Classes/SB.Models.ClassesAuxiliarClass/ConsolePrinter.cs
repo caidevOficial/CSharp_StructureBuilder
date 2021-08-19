2384daefbd9d88af162ee163d897cb5c17e7189d
CaidevOficial <falcone.facu@gmail.com>
2021-08-19 02:19:29 -0300

    Advanced to version V2.6.1.0

---
+++ StructureBuilder/Entities/SB.Models.Classes/SB.Models.ClassesAuxiliarClass/ConsolePrinter.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+
+namespace AuxiliarClass {
+    public sealed class ConsolePrinter {
+
+        #region Printers
+
+        /// <summary>
+        /// Prints characters.
+        /// </summary>
+        /// <param name="length">Max length of the line.</param>
+        /// <param name="charact">Character or symbol to print</param>
+        /// <param name="foregroundColor">Color Of the symbols</param>
+        private static void PrintSymbols(int length, char charact, ConsoleColor foregroundColor) {
+            Console.ForegroundColor = foregroundColor;
+            for (int index = 0; index < length; index++) {
+                Console.Write(charact);
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Prints the title and decorations of the program.
+        /// </summary>
+        /// <param name="title">Title to print.</param>
+        /// <param name="titleForegroundColor">Color of the Text</param>
+        /// <param name="symbolForegroundColor">Color Of the symbols</param>
+        public static void PrintHeader(string title, ConsoleColor titleForegroundColor, ConsoleColor symbolForegroundColor) {
+            int length = title.Length;
+            int lengthLine = (length);
+
+            PrintSymbols(lengthLine, '=', symbolForegroundColor);
+            Console.ForegroundColor = titleForegroundColor;
+            Console.WriteLine(title);
+            PrintSymbols(lengthLine, '=', symbolForegroundColor);
+        }
+
+        /// <summary>
+        /// Prints the subHeader of the program.
+        /// </summary>
+        /// <param name="subHeader">Subheader to print.</param>
+        /// <param name="titleForegroundColor">Color of the Text</param>
+        /// <param name="symbolForegroundColor">Color Of the symbols</param>
+        public static void PrintSubHeader(string subHeader, ConsoleColor titleForegroundColor, ConsoleColor symbolForegroundColor) {
+            int subLength = subHeader.Length;
+            Console.ForegroundColor = titleForegroundColor;
+            Console.WriteLine($"\n{subHeader}\n");
+            PrintSymbols(subLength, '-', symbolForegroundColor);
+        }
+
+        #endregion
+
+        #region CalculateAndPrints
+
+        /// <summary>
+        /// Calculates the percentage of the program.
+        /// </summary>
+        /// <param name="baseNumber">Max number of steps.</param>
+        /// <param name="top">Actual number of steps done.</param>
+        /// <returns></returns>
+        private static double PercentageCalc(short baseNumber, short top) {
+            int result;
+            result = (100 * top) / baseNumber;
+            return result;
+        }
+
+        /// <summary>
+        /// Shows the progress of the program.
+        /// </summary>
+        /// <param name="baseNumber">Max number of steps.</param>
+        /// <param name="top">Actual number of steps done.</param>
+        public static void ShowProgress(short baseNumber, short top) {
+            PrintHeader("Structure Builder", ConsoleColor.Yellow, ConsoleColor.Green);
+            PrintSubHeader("Step 3: Building Struct Files", ConsoleColor.Yellow, ConsoleColor.Green);
+            Console.Write($"\nProgress: [{PercentageCalc(baseNumber, top)}]%\n");
+        }
+
+        #endregion
+
+    }
+}
---
+++ StructureBuilder/Entities/SB.Models.Classes/SB.Models.ClassesEntities/Parameter.cs
@@ -0,0 +1,197 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2021 [FacuFalcone]
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using AuxiliarClass;
+
+namespace Entities {
+    public sealed class Parameter {
+
+        #region Attributes
+
+        private short idParameter;
+        private int lengthParameter;
+        private string typeParameter;
+        private string nameParameter;
+        private string aliasNameParameter;
+
+        #endregion
+
+        #region Builders
+
+        /// <summary>
+        /// Builds the entity without parameters.
+        /// </summary>
+        public Parameter() { }
+
+        /// <summary>
+        /// Builds the entity with the id.
+        /// </summary>
+        /// <param name="idParameter">ID/PK of the entity.</param>
+        public Parameter(short idParameter) : this() {
+            IdParameter = idParameter;
+        }
+
+        /// <summary>
+        /// Builds the entity with the name and the id.
+        /// </summary>
+        /// <param name="idParameter">ID/PK of the entity.</param>
+        /// /// <param name="nameParameter">Name of the parameter.</param>
+        public Parameter(short idParameter, string nameParameter)
+            : this(idParameter) {
+            this.NameParameter = nameParameter;
+        }
+
+        /// <summary>
+        /// Builds the entity with the name, id and type.
+        /// </summary>
+        /// <param name="idParameter">ID/PK of the entity.</param>
+        /// <param name="nameParameter">Name of the parameter.</param>
+        /// <param name="typeParameter">Type of the parameter.</param>
+        public Parameter(short idParameter, string nameParameter, string typeParameter)
+            : this(idParameter, nameParameter) {
+            this.TypeParameter = typeParameter;
+        }
+
+        /// <summary>
+        /// Builds the entity with the id, name type and the length.
+        /// </summary>
+        /// <param name="idParameter">ID/PK of the entity.</param>
+        /// <param name="nameParameter">Name of the parameter.</param>
+        /// <param name="typeParameter">Type of the parameter.</param>
+        /// <param name="lengthParameter">Length of the parameter.</param>
+        public Parameter(short idParameter, string nameParameter, string typeParameter, int lengthParameter)
+            : this(idParameter, nameParameter, typeParameter) {
+            this.LengthParameter = lengthParameter;
+        }
+
+        /// <summary>
+        /// Builds the entity with the id, name type, the length and the alias.
+        /// </summary>
+        /// <param name="idParameter">ID/PK of the entity.</param>
+        /// <param name="nameParameter">Name of the parameter.</param>
+        /// <param name="typeParameter">Type of the parameter.</param>
+        /// <param name="lengthParameter">Length of the parameter.</param>
+        /// <param name="aliasNameParameter">Alias - Short name of the parameter.</param>
+        public Parameter(short idParameter, string nameParameter, string typeParameter, int lengthParameter, string aliasNameParameter)
+            : this(idParameter, nameParameter, typeParameter, lengthParameter) {
+            this.AliasNameParameter = aliasNameParameter;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Get: Gets the type of the parameter.
+        /// Set: Sets the type of the parameter.
+        /// </summary>
+        public string TypeParameter {
+            get => this.typeParameter;
+            set {
+                if (!(value is null)) {
+                    typeParameter = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get: Gets the name of the parameter.
+        /// Set: Sets the name of the parameter.
+        /// </summary>
+        public string NameParameter {
+            get => nameParameter;
+            set {
+                if (!(value is null)) {
+                    nameParameter = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get: Gets the alias of the parameter.
+        /// Set: Sets the alias of the parameter.
+        /// </summary>
+        public string AliasNameParameter {
+            get => aliasNameParameter;
+            set {
+                if (!(value is null)) {
+                    aliasNameParameter = DataCollector.ParameterAliasCollector(value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get: Gets the length of the parameter.
+        /// Set: Sets the length of the parameter.
+        /// </summary>
+        public int LengthParameter {
+            get => lengthParameter;
+            set {
+                if (value > 0) {
+                    lengthParameter = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get: Gets the id of the parameter.
+        /// Set: Sets the id of the parameter.
+        /// </summary>
+        public short IdParameter {
+            get => idParameter;
+            set {
+                if (value > 0) {
+                    idParameter = value;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Operators
+
+        /// <summary>
+        /// Checks if both parameters are equals.
+        /// </summary>
+        /// <param name="p1">First parameter to check.</param>
+        /// <param name="p2">Second parameter to check.</param>
+        /// <returns>True if both are equals, otherwise returns false.</returns>
+        public static bool operator ==(Parameter p1, Parameter p2) {
+            return p1.IdParameter == p2.IdParameter;
+        }
+
+        /// <summary>
+        /// Checks if both parameters aren't equals.
+        /// </summary>
+        /// <param name="p1">First parameter to check.</param>
+        /// <param name="p2">Second parameter to check.</param>
+        /// <returns>True if both aren't equals, otherwise returns false.</returns>
+        public static bool operator !=(Parameter p1, Parameter p2) {
+            return !(p1 == p2);
+        }
+
+        #endregion
+
+    }
+}
